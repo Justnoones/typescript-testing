fc17b51f6650a69952e3dba497395251d3daa1c9
Justnoone <yanw97841@gmail.com>
2023-10-12 14:10:54 +0630

    type-literal

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,3 @@
 "use strict";
-var lbsToString = (weight) => {
-    if (typeof weight === 'number')
-        return weight * 2.2;
-    else
-        return parseInt(weight) / 2.2;
-};
-console.log(lbsToString(30));
-console.log(lbsToString('30kg'));
+let quantity = 50;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-var lbsToString = (weight: number | string): number => {
-    if (typeof weight === 'number')
-        return weight * 2.2
-    else 
-        return parseInt(weight) / 2.2
-}
+type Quantity = 50 | 100
 
-console.log(lbsToString(30))
-console.log(lbsToString('30kg'))
+let quantity: Quantity = 50
